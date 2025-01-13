36eaf1ce0bbe45af5855bb320541335b12b83cf1
Ni0duann <2014567981@qq.com>
2025-01-13 10:03:38 +0800

    feat: finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createFromIconfont } from "./Icon/createFrontIconfont";
+
+const IconFont = createFromIconfont(
+  "//at.alicdn.com/t/c/font_4443338_a2wwqhorbk4.js"
+);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div style={{ padding: "50px" }}>
+        <IconFont type="icon-shouye-zhihui" size="40px"></IconFont>
+        <IconFont
+          type="icon-gerenzhongxin-zhihui"
+          fill="blue"
+          size="40px"
+        ></IconFont>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
